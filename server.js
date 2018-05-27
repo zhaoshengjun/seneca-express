11614aab8a6c23a482db9e4ebe6fa58e6d1a7c80
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-27 22:02:56 +1000

    use add pattern

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const seneca = require("seneca")();
 
-seneca.act({ cmd: "register", role: "accountManagement" }, (err, result) => {
-  if (err) console.error(err);
-  console.log(`register response: ${result.message} `);
+seneca.add({ role: "accountManagement", cmd: "register" }, (msg, respond) => {
+  respond(null, {
+    message: `registering ${msg.username}`
+  });
 });
 
-seneca.act({ cmd: "login", role: "accountManagement" }, (err, result) => {
-  if (err) console.error(err);
-  console.log(`register response: ${result.message} `);
+seneca.add({ role: "accountManagement", cmd: "login" }, (msg, respond) => {
+  respond(null, {
+    message: `logging in ${msg.username}`
+  });
 });
+
+seneca.act(
+  { cmd: "register", role: "accountManagement", username: "testuser" },
+  (err, result) => {
+    if (err) console.error(err);
+    console.log(`register response: ${result.message} `);
+  }
+);
+
+seneca.act(
+  { cmd: "login", role: "accountManagement", username: "testuser" },
+  (err, result) => {
+    if (err) console.error(err);
+    console.log(`register response: ${result.message} `);
+  }
+);
